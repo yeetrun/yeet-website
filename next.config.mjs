66901e88d9a10e6cca7e3868748970c6dd29acf2
shayne <79330+shayne@users.noreply.github.com>
2026-04-03 12:32:05 -0400

    next.config: noindex only on dev server

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
+
+/** @type {(phase: string) => import('next').NextConfig} */
+const nextConfig = (phase) => ({
   reactStrictMode: true,
   outputFileTracingIncludes: {
     '*': ['docs/**'],
@@ -7,6 +9,24 @@ const nextConfig = {
   env: {
     GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF || '',
   },
-};
+
+  async headers() {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+      return [
+        {
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex',
+            },
+          ],
+          source: '/:path*',
+        },
+      ];
+    }
+
+    return [];
+  },
+});
 
 export default nextConfig;
